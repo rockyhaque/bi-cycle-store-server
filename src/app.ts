8b3a8294824afd0b709e341f4df1d3aee1334a93
Rocky Haque <rockyhaque99@gmail.com>
2025-02-08 00:02:00 +0600

    Product Management done

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,17 @@
 import express, { Request, Response } from 'express'
 import productRouter from './module/product/product.route'
 import orderRouter from './module/order/order.route'
+import { globalErrorHandler } from './middlewares/globalErrorHandler'
+import userRouter from './module/user/user.route'
+import authRouter from './module/auth/auth.route'
 
 const app = express()
 
 // middleware
 app.use(express.json())
 
+app.use('/api/auth', authRouter)
+app.use('/api/user', userRouter)
 app.use('/api/products', productRouter)
 app.use('/api/orders', orderRouter)
 
@@ -16,5 +21,15 @@ app.get('/', (req: Request, res: Response) => {
     message: 'Bi Cycle Store Server is running',
   })
 })
+
+app.use('*', (req: Request, res: Response) => {
+  res.status(404).json({
+    success: false,
+    message: 'Route not found',
+  })
+})
+
+// Global Error Handler
+app.use(globalErrorHandler)
 
 export default app
